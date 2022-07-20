bdc2e8506ef5e09f4cc93ebf7d586149da137299
Mateusz <63960220+MateuszSamin@users.noreply.github.com>
2022-07-20 17:17:59 +0200

    add mobile versions

--- components/blocks/numbers.tsx
+++ components/blocks/numbers.tsx
@@ -6,17 +6,17 @@ export const Numbers = () =>{
     return(
             <Section className="bg-parlourDark">
                 <Container className="h-screen flex items-center justify-center">
-                    <div className=" flex flex-row items-end justify-center gap-44">
+                    <div className=" flex lg:flex-row flex-col lg:items-end items-center justify-center lg:gap-44 gap-10">
                         <div className="flex flex-col items-center">
-                            <span className="text-transparent bg-clip-text break-words bg-gradient-to-r bg-radial-at-tl from-parlourGreen to-parlourBlue text-4xl font-bold">24</span>
+                            <span className="text-transparent bg-clip-text break-words bg-gradient-to-r bg-radial-at-tl from-parlourGreen to-parlourBlue lg:text-4xl text-3xl font-bold">24</span>
                             <p className="text-white text-base font-bold">active customers</p>
                         </div>
                         <div className="flex flex-col items-center">
-                            <span className="text-transparent bg-clip-text break-words bg-gradient-to-r bg-radial-at-tl from-sky-400 to-purple-700 text-6xl font-bold">365</span>
+                            <span className="text-transparent bg-clip-text break-words bg-gradient-to-r bg-radial-at-tl from-sky-400 to-purple-700 lg:text-6xl text-5xl font-bold">365</span>
                             <p className="text-white text-xl font-bold">days per year</p>
                         </div>
                         <div className="flex flex-col items-center">
-                            <span className="text-transparent bg-clip-text break-words bg-gradient-to-r bg-radial-at-tl from-parlourBlue to-blue-500 text-4xl font-bold">92</span>
+                            <span className="text-transparent bg-clip-text break-words bg-gradient-to-r bg-radial-at-tl from-parlourBlue to-blue-500 lg:text-4xl text-3xl font-bold">92</span>
                             <p className="text-white text-base font-bold">instantly fixed crashes</p>
                         </div>
                     </div>
--- pages/parlourCare.tsx
+++ pages/parlourCare.tsx
@@ -4,22 +4,30 @@ import { Container } from "../components/util/container";
 import { Section } from "../components/util/section";
 import CareIMG from "../public/parlour-care1.png"
 import ParlourCarePlus from "../public/uploads/parlourcareplus1.png"
+import ParlourCarePlusOutline from "../public/uploads/parlourcareplusoutline1.png"
 
 export default function ParlourCarePage(){
     return(
         <Layout>
             <Section>
-                <Container className="h-screen">
-                    <div className="text-parlourDark text-6xl font-bold flex flex-col items-center">
+                <Container className="lg:h-screen h-[80vh]">
+                    <div className="text-parlourDark lg:text-6xl text-3xl font-bold flex flex-col items-center z-30">
                         <div className="mt-32">
                             <div className="flex flex-row gap-10 items-center">
                                 <h1>Fully</h1>
-                                <img src={ParlourCarePlus.src} className="w-36 h-36" />
+                                <img src={ParlourCarePlus.src} className="lg:w-36 lg:h-36 w-20 h-20" />
                             </div>
                             <h1>Covered.</h1>
                         </div>
-                        <img src={CareIMG.src} className="mt-32" />
-                        <img src="" alt="" />
+                        <img src={CareIMG.src} className="mt-24 lg:w-64 w-36" />
+                        <img src={ParlourCarePlusOutline.src} className="absolute top-96 right-5 lg:block hidden" />
+                        <img src={ParlourCarePlusOutline.src} className="lg:w-20 absolute top-28 right-24 lg:block hidden" />
+                        <img src={ParlourCarePlusOutline.src} className="lg:w-14 absolute top-64 right-[20%] lg:block hidden" />
+                        <img src={ParlourCarePlusOutline.src} className="w-32 absolute top-28 right-[30%] lg:block hidden" />
+
+                        <img src={ParlourCarePlusOutline.src} className="lg:w-52 absolute top-[65vh] left-14 lg:block hidden" />
+                        <img src={ParlourCarePlusOutline.src} className="lg:w-14 absolute top-72 left-20 lg:block hidden" />
+                        <img src={ParlourCarePlusOutline.src} className="lg:w-32 absolute top-28 left-[15%] lg:block hidden" />
                     </div>
                 </Container>
             </Section>
